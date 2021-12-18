00d6292e45ac876b1d8ba40a8dd4234beea2d508
Brevard89 <89888435+Brevard89@users.noreply.github.com>
2021-12-17 21:17:57 -0500

    Added Folders second time, First attempt failed

--- Linux/sys_info.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-echo "A Quick System Audit Script"
-echo "Todays date is  October 13th 2021"
-echo "Machine Type Info"
-echo "$MACHTYPE"
-echo -e "Uname info: $(uname -a) \n"
-echo -e "IP Info: $(ip addr | head -9 | tail -1) \n"
